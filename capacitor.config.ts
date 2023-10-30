a177aa20f3cb150db90166e7510f59c652c3f460
Iknowthereason <92931371+Iknowthereason@users.noreply.github.com>
2023-10-30 10:57:34 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Cobysproject',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
